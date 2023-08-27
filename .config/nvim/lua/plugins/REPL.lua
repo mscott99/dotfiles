76d07da79be6e6d004fdc2a3a97acb14082e4d4a
Matthew Scott <m.valckescott@gmail.com>
2023-08-27 10:13:32 -0700

    ignoring files

--- .config/nvim/lua/plugins/REPL.lua
+++ .config/nvim/lua/plugins/REPL.lua
@@ -6,26 +6,26 @@ return {
     -- event = "VeryLazy",
     -- ft = { "python", "julia" },
     -- event = "BufEnter *.jl",
-    ft = { "julia" },
+    -- ft = { "julia" },
     -- event = { "julia" },
-    config = function()
-      -- vim.cmd([[set nocompatible]])
-      -- vim.cmd([[filetype off]])
-
-      -- vim.cmd([[let &runtimepath.=',~/.vim/bundle/neoterm']])
-
-      -- Enable below for python
-      -- vim.g.neoterm_repl_python = { "source ./myenv/bin/activate", "ipython" }
-      -- vim.g.neoterm_repl_command = { "", "" }
-      -- vim.g.neoterm_default_mod = "belowright"
-
-      vim.g.neoterm_default_mod = "belowright"
-      vim.g.neoterm_size = 16
-      vim.g.neoterm_autoscroll = 1
-      vim.keymap.set("n", "<leader>r", ":TREPLSendLine<cr>j")
-      vim.keymap.set("n", "<leader>t", "Vip:TREPLSendLine<cr>j")
-      vim.keymap.set("v", "<leader>r", ":TREPLSendSelection<cr>j")
-      vim.keymap.set("i", "<C-d>", "<cmd>TREPLSendSelection<cr>")
-    end,
+    -- config = function()
+    --   -- vim.cmd([[set nocompatible]])
+    --   -- vim.cmd([[filetype off]])
+    --
+    --   -- vim.cmd([[let &runtimepath.=',~/.vim/bundle/neoterm']])
+    --
+    --   -- Enable below for python
+    --   -- vim.g.neoterm_repl_python = { "source ./myenv/bin/activate", "ipython" }
+    --   -- vim.g.neoterm_repl_command = { "", "" }
+    --   -- vim.g.neoterm_default_mod = "belowright"
+    --
+    --   vim.g.neoterm_default_mod = "belowright"
+    --   vim.g.neoterm_size = 16
+    --   vim.g.neoterm_autoscroll = 1
+    --   vim.keymap.set("n", "<leader>r", ":TREPLSendLine<cr>j")
+    --   vim.keymap.set("n", "<leader>t", "Vip:TREPLSendLine<cr>j")
+    --   vim.keymap.set("v", "<leader>r", ":TREPLSendSelection<cr>j")
+    --   vim.keymap.set("i", "<C-d>", "<cmd>TREPLSendSelection<cr>")
+    -- end,
   },
 }
