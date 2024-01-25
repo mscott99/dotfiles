d7566c617a6af32611d7a686832d37ad16c2426f
Matthew Scott <m.valckescott@gmail.com>
2024-01-24 17:18:58 -0800

    better visual REPL

--- .config/nvim/lua/plugins/REPL.lua
+++ .config/nvim/lua/plugins/REPL.lua
@@ -11,14 +11,24 @@ local start_repl = function()
 end
 
 local trim_spaces = false
-vim.keymap.set({"n", "v"}, "<leader>R", start_repl, { desc = "start repl" })
+vim.keymap.set({ "n", "v" }, "<leader>R", start_repl, { desc = "start repl" })
 vim.keymap.set("n", "<leader>r", function()
   require("toggleterm").send_lines_to_terminal("single_line", trim_spaces, { args = vim.v.count })
-  vim.cmd('normal! j')
+  vim.cmd("normal! j")
 end)
+vim.keymap.set({ "n", "i" }, "®", function()
+  require("toggleterm").send_lines_to_terminal("single_line", trim_spaces, { args = vim.v.count })
+  vim.cmd("normal! j")
+end)
+
 vim.keymap.set("v", "<leader>r", function()
-  require("toggleterm").send_lines_to_terminal("visual_lines", trim_spaces, { args = vim.v.count })
-  vim.cmd('normal! }')
+  local tog = require("toggleterm")
+  if vim.api.nvim_get_mode().mode == "v" then
+    tog.send_lines_to_terminal("visual_selection", trim_spaces, { args = vim.v.count })
+  elseif vim.api.nvim_get_mode().mode == "V" then
+    tog.send_lines_to_terminal("visual_lines", trim_spaces, { args = vim.v.count })
+  end
+  vim.cmd("normal! `>") -- move to end of visual selection
 end)
 
 return {
